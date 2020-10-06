32471ea5878c45e846b015cadba472161daf7db0
Gustavo Vergara <gustavo.garcia@corp.globo.com>
2020-10-06 15:35:44 -0300

    Initial Commit

---
+++ CoordinatorExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorExample
+//
+//  Created by Gustavo Vergara Garcia on 06/10/20.
+//  Copyright © 2020 Gustavo Vergara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
